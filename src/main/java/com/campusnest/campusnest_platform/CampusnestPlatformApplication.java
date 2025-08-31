643f516befa2bd5d96126dfd292372f08e5562b0
M1ngdaXie <156019134+M1ngdaXie@users.noreply.github.com>
2025-08-31 12:53:38 -0700

    initial commit

---
+++ src/main/java/com/campusnest/campusnest_platform/CampusnestPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.campusnest.campusnest_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusnestPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusnestPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/campusnest/campusnest_platform/CampusnestPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campusnest.campusnest_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusnestPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
